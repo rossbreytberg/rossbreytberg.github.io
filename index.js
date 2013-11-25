2937b47c4a621d597a04c9058b33381955368d75
Ross Breytberg <rossbreytberg@eos.hsd1.ca.comcast.net>
2013-11-24 17:44:18 -0800

    updated navigation again

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-var onChangePage = function(response) {
+var DEFAULT_PAGE = 'about';
+
+// Given an HTML string response, set it as the page content.
+var onChangePage = function(/*string*/ response) {
   var body = document.getElementById('body');
   body.innerHTML = response;
 }
 
-window.onhashchange = function(event) {
-  var hash = window.location.hash.replace('#', '');
+// Given a URL, load the contents into the body of the page.
+var loadPage = function(/*string*/ url) {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
       onChangePage(request.responseText);
     }
   };
-  request.open('GET', hash + '.html', true);
+  request.open('GET', url, true);
   request.send();
 };
 
-window.onload = function() {
-  var nav_links = document.getElementById('nav').getElementsByTagName('a');
+var loadPageFromCurrentHash = function() {
+  var hash = window.location.hash.replace('#', '') || DEFAULT_PAGE;
 
-  var onClickNavLink = function(event) {
-    for (var i = 0; i < nav_links.length; i++) {
-      nav_links[i].classList.remove('selected');
+  var links = {
+    about: document.getElementById('nav-about'),
+    projects: document.getElementById('nav-projects')
+  };
+
+  for (var link_name in links) {
+    if (links.hasOwnProperty(link_name)) {
+      if (link_name === hash) {
+        links[link_name].classList.add('selected');
+      } else {
+        links[link_name].classList.remove('selected');
+      }
     }
-    event.target.classList.add('selected');
   }
 
-  for (var i = 0; i < nav_links.length; i++) {
-    nav_links[i].onclick = onClickNavLink;
-  }
+  loadPage(hash + '.html');
 }
+
+window.onhashchange = loadPageFromCurrentHash;
+window.onload = loadPageFromCurrentHash;
